c8782fa6f4bdafe6eb1e7fa44d6ece2e00a258e5
Miguel A. Munoz <magonzalez@fortinet.com>
2021-07-12 15:42:41 +0200

    loop

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-EICAR
-=====
-wget http://www.rexswain.com/eicar.com
-wget http://www.rexswain.com/eicar.zip
-wget http://www.rexswain.com/eicar2.zip
+
+while [1]; do
+
+    wget http://www.rexswain.com/eicar.com
+    wget http://www.rexswain.com/eicar.zip
+    wget http://www.rexswain.com/eicar2.zip
+
+    wget https://secure.eicar.org/eicar.com
+
+done
