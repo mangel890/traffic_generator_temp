4ca4e351ce72128fb549ebe5407e99ec9ce35893
Miguel A. Munoz <magonzalez@fortinet.com>
2021-07-13 19:06:53 +0200

    t54

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ NC="\e[0m"
 wget_options='-qO- -T 30 -t 1'
 curl_options='-s -o /dev/null'
 
+#=============================
+# Initial connectivity
+#=============================
 echo
 echo -e "${YEL}General connectivity tests${NC}"
 echo "=========================="
@@ -22,6 +25,9 @@ else
 fi
 echo "(1/1)."
 
+#=============================
+# EICAR (AV/IPS)
+#=============================
 echo
 echo "Downloading EICAR (3):"
 wget ${wget_options} http://www.rexswain.com/eicar.com > /dev/null
@@ -46,6 +52,9 @@ else
 fi
 echo "(3/3)."
 
+#=============================
+# Files (MP3)
+#=============================
 echo
 echo "Downloading MP3 files (2):"
 timeout 30 wget ${wget_options} http://www.gurbaniupdesh.org/multimedia/01-Audio%20Books/Baba%20Noadh%20Singh/000%20Introduction%20Bhai%20Sarabjit%20Singh%20Ji%20Gobindpuri.mp3  > /dev/null
@@ -64,6 +73,9 @@ else
 fi
 echo "(2/2)."
 
+#=============================
+# DLP
+#=============================
 echo
 echo "Checking DLP(4)"
 echo "  Credit Card (Amex):"
@@ -102,6 +114,9 @@ else
 fi
 echo "(4/4)."
 
+#=============================
+# Viruses
+#=============================
 echo
 echo "Downloading Viruses (2):"
 wget ${wget_options} http://www.esthetique-realm.net/ > /dev/null
@@ -119,6 +134,17 @@ else
 fi
 echo "(2/2)."
 
-
+#=============================
+# WebFilter
+#=============================
+echo
+echo "Checking WebFilter (5):"
+wget ${wget_options} www.magikmobile.com > /dev/null
+if [ $? -ne 0 ]; then
+    echo -ne "${GRE}Ok:${NC} magikmobile cannot be accessed"
+else
+    echo -ne "${RED}Error:${ENDCOLOR} magikmobile can be accessed"
+fi
+echo "(1/5)."
 
 # wget --no-check-certificate https://secure.eicar.org/eicar.com
