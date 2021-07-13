1e3cd3e3b47ce558c519f807b988ef3c1324b9c6
Miguel A. Munoz <magonzalez@fortinet.com>
2021-07-13 17:15:46 +0200

    gr

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@
 wget_options='-qO- -T 30 -t 1'
 
 RED="\e[31m"
+GRE="\e[32m"
 NC="\e[0m"
 
+echo
+echo "General connectivity tests"
+echo "=========================="
 echo
 echo "Checking internet connectivity (1):"
 wget $wget_options https://www.google.com > /dev/null
@@ -12,7 +16,7 @@ wget $wget_options https://www.google.com > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${RED}Error:${NC} Cannot get any traffic. Accessing Google.com failed"
 else
-    echo -ne "Google.com can be accessed. Internet connectivity"
+    echo -ne "${GRE}Ok:${NC} Google.com can be accessed. Internet connectivity seems to be ok"
 fi
 echo "(1/1)."
 
@@ -20,21 +24,21 @@ echo
 echo "Downloading EICAR (3):"
 wget $wget_options http://www.rexswain.com/eicar.com > /dev/null
 if [ $? -ne 0 ]; then
-    echo -ne "Ok: EICAR cannot be downloaded"
+    echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
     echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
 fi
 echo "(1/3)."
 wget $wget_options http://www.rexswain.com/eicar.zip > /dev/null
 if [ $? -ne 0 ]; then
-    echo -ne "Ok: EICAR cannot be downloaded"
+    echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
     echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
 fi
 echo "(2/3)."
 wget $wget_options http://www.rexswain.com/eicar2.zip > /dev/null
 if [ $? -ne 0 ]; then
-    echo -ne "Ok: EICAR cannot be downloaded"
+    echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
     echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
 fi
@@ -46,7 +50,7 @@ timeout 30 wget $wget_options http://www.gurbaniupdesh.org/multimedia/01-Audio%2
 if [ $? -ne 0 ]; then
     echo -ne "${RED}Error:${NC} Cannot download MP3 files"
 else
-    echo -ne "MP3 file can be downloaded normally"
+    echo -ne "${GRE}Ok:${NC} MP3 file can be downloaded normally"
 fi
 echo "(1/2)."
 
@@ -54,7 +58,7 @@ timeout 30 wget $wget_options http://www.theradiodept.com/media/mp3/david.mp3 > 
 if [ $? -ne 0 ]; then
     echo -ne "${RED}Error:${NC} Cannot download MP3 files"
 else
-    echo -ne "MP3 file can be downloaded normally"
+    echo -ne "${GRE}Ok:${NC} MP3 file can be downloaded normally"
 fi
 echo "(2/2)."
 
@@ -63,7 +67,7 @@ echo "Checking DLP(2)"
 
 curl -s -o /dev/null -X POST http://dlptest.com/http-post -H "Content-Type:multipart/form-data; boundary=---------------------------52410911313245418552292478843" -d '-----------------------------52410911313245418552292478843\nContent-Disposition: form-data; name="item_meta[6]"\n\n371193356045439\n-----------------------------52410911313245418552292478843'
 if [ $? -ne 0 ]; then
-    echo -ne "Cannot leak data"
+    echo -ne "${GRE}Ok:${NC} Cannot leak data"
 else
     echo -ne "${RED}Error:${NC} Data seems to be leaked"
 fi
